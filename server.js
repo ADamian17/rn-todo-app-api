0ae425297196362da9880621d0baa241c93497d1
ADamian17 <adonisdesuvikdo@gmail.com>
2022-01-23 23:05:46 -0800

    deploy 1

--- server.js
+++ server.js
@@ -5,6 +5,8 @@ const server = http.Server(app);
 const { Server } = require("socket.io");
 const io = new Server(server);
 
+const PORT = process.env.PORT || 3000
+
 app.get('/', (req, res) => {
   res.render('index.ejs');
 });
@@ -15,6 +17,6 @@ io.on('connection', (socket) => {
   });
 });
 
-server.listen(3000, () => {
+server.listen(PORT, () => {
   console.log('listening on *:3000');
 });
